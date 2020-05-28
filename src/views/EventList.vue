bbea0da9295710d28750eca28309ccd92464736e
Pardip Bhatti <pardipbhatti8791@gmail.com>
2020-05-29 03:07:27 +0530

    inital commit

---
+++ src/views/EventList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Event List</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+import EventService from '@/services/EventService.js'
+import EventCard from '@/components/EventCard'
+
+export default {
+  components: {
+    EventCard: EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    EventService.getEvents()
+      .then(response => {
+        this.events = response.data
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
+}
+</script>
