1c772a6d7344f15fd812ef595e100729d763e3f5
Pardip Bhatti <pardipbhatti8791@gmail.com>
2020-05-31 15:30:13 +0530

    vuex state management

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,57 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    user: { id: 'gpgugu', name: 'gugu meri' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false },
+      { id: 3, text: '...', done: true },
+      { id: 4, text: '...', done: false }
+    ],
+    events: [
+      { id: 1, title: '...', organizer: true },
+      { id: 2, title: '...', organizer: false },
+      { id: 3, title: '...', organizer: true },
+      { id: 4, title: '...', organizer: false }
+    ],
+    count: 1
+  },
+  mutations: {
+    INCREMENT_COUNT(state, value) {
+      state.count += value
+    }
+  },
+  actions: {
+    updateCount({ state, commit }, value) {
+      if (state.user) {
+        commit('INCREMENT_COUNT', value)
+      }
+    }
+  },
+  modules: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length
+    },
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    activeTodosCount: state => {
+      // return state.todos.length - getters.doneTodos.length
+      return state.todos.filter(todo => !todo.done).length
+    },
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    }
+  }
 })
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,49 @@
 <template>
-  <h1>Event Create</h1>
+  <div>
+    <div>Create an event, {{ user.name }}</div>
+    <p>This event is created by: {{ user.id }}</p>
+    <p>There are {{ catLength }} categories</p>
+    <p>{{ getEvent(1) }}</p>
+    <input type="number" v-model.number="incrementBy" />
+    <p>counter: {{ getCount }}</p>
+    <button @click="incrementCount">Increment</button>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data() {
+    return {
+      incrementBy: 1
+    }
+  },
+  // computed: mapState({
+  //   user: 'user',
+  //   categories: 'categories'
+  // })
+  methods: {
+    incrementCount() {
+      this.$store.dispatch('updateCount', this.incrementBy)
+    }
+  },
+  computed: {
+    catLength() {
+      return this.$store.getters.catLength
+    },
+    ...mapState(['user', 'categories']),
+    getEvent() {
+      return this.$store.getters.getEventById
+    },
+    getCount() {
+      return this.$store.state.count
+    }
+  }
+}
+</script>
+
+<style scoped></style>
